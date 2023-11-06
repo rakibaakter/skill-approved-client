f48c17522bb3796d8e28e8e3d197b881d2a58405
rakibaakter <rakibaakter635@gmail.com>
2023-11-07 00:39:40 +0600

    implemented framer motion to hamburger menu

--- src/Component/MenuBar/MenuBar.jsx
+++ src/Component/MenuBar/MenuBar.jsx
@@ -1,8 +1,17 @@
 import { GiHamburgerMenu } from "react-icons/gi";
 import { Link, NavLink } from "react-router-dom";
 import Logo from "../Logo/Logo";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 const MenuBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const variants = {
+    open: { opacity: 1, x: 0 },
+    closed: { opacity: 1, x: "-100%" },
+  };
+
   const listItems = (
     <>
       <li>
@@ -28,18 +37,20 @@ const MenuBar = () => {
     <div className="navbar bg-cyan-500 text-white fixed px-2 md:px-10 lg:px-20">
       <div className="navbar-start">
         <div className="dropdown">
-          <label
-            tabIndex={0}
-            className="btn btn-ghost text-cyan-700 text-2xl lg:hidden"
-          >
-            <GiHamburgerMenu />
+          <label tabIndex={0} className=" text-cyan-700 text-2xl lg:hidden">
+            <motion.nav
+              animate={isOpen ? "open" : "closed"}
+              variants={variants}
+            >
+              <GiHamburgerMenu onClick={() => setIsOpen((isOpen) => !isOpen)} />
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-xl text-black"
+              >
+                {listItems}
+              </ul>
+            </motion.nav>
           </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-xl text-black"
-          >
-            {listItems}
-          </ul>
         </div>
         <Logo />
       </div>
